daa0f105f60fb61547393f6ee635f77f35b3b319
Adé Mochtar <amochtar@xebia.com>
2015-03-06 15:39:51 +0100

    Update service wrapper for registering and deregistering service with consul

--- servicewrapper/servicewrapper.sh
+++ servicewrapper/servicewrapper.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
-export CONTAINERIP=$(hostname  -I | cut -f1 -d' ')
+export CONTAINERIP=$(hostname -I | cut -f1 -d' ')
 
+function deregister() {
+    echo "Trapping exit"
+    curl -X POST http://consul.service.consul:8500/v1/agent/service/deregister/helloworld-$HOSTNAME \
+        --header 'Content-Type: application/json'
+    kill -SIGTERM -1
+}
+trap "deregister" EXIT
+
+# start app
+python /srv/helloworld.py &
+
+# register with consul
 curl -X POST http://consul.service.consul:8500/v1/agent/service/register \
-  --header 'Content-Type: application/json' \
-  --data-binary '{"ID": "'"helloworld-$HOSTNAME"'", "Name": "helloworld", "Address": "'"$CONTAINERIP"'", "Port": 80, "Check": {"HTTP": "http://'"$CONTAINERIP"'", "Interval": "30s"}}'
+    --header 'Content-Type: application/json' \
+    --data-binary '{"ID": "'"helloworld-$HOSTNAME"'", "Name": "helloworld", "Address": "'"$CONTAINERIP"'", "Port": 80, "Check": {"HTTP": "http://'"$CONTAINERIP"'", "Interval": "30s"}}'
+
+while true; do :; done
