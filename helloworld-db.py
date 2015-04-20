9f656a58a05a9803884777527d90f7808fa43e00
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-04-20 12:36:12 +0200

    added logging and error handling

--- helloworld-db.py
+++ helloworld-db.py
@@ -2,13 +2,29 @@
 
 from flask import Flask
 from redis import Redis
+import os
+import logging
+import logging.handlers
+
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    try:
+       redis.incr('hits')
+       hits = redis.get('hits')
+       app.logger.info("hello called %s times" % hits)
+       return 'Hello World! I have been seen %s times.\n' % hits
+    except Exception as e:
+       app.logger.error(str(e))
+       return "Hello World! Cannot tell you how many times I have been seen.\n\t(%s)\n" % str(e)
 
 if __name__ == "__main__":
+    if os.path.exists('/dev/log'):
+      handler = logging.handlers.SysLogHandler(address = '/dev/log')
+    else:
+      handler = logging.handlers.SysLogHandler()
+    handler.setLevel(logging.ERROR)
+    app.logger.addHandler(handler)
     app.run(host="0.0.0.0", port=80, debug=True)
